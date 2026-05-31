acb9f9ee90c9cdafe6c961c446f9677976e70d6b
Arif14377 <arifrahman14377@gmail.com>
2026-05-31 13:05:06 +0700

    feat: add constraint uniqueIndex for permission name

--- backend/models/permission.go
+++ backend/models/permission.go
@@ -4,7 +4,7 @@ import "time"
 
 type Permission struct {
 	Id        uint      `json:"id" gorm:"primaryKey"`
-	Name      string    `json:"name"`
+	Name      string    `json:"name" gorm:"type:varchar(100);uniqueIndex;not null"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
