f3b93e57df18693eb9a1576ea30dfe63d2650a0a
Arif14377 <arifrahman14377@gmail.com>
2026-08-01 21:32:17 +0700

    feat: add useCreateAddress hook for creating addresses

---
+++ frontend/src/hooks/web/address/useCreateAddress.ts
@@ -0,0 +1,17 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import Api from '../../../services/api.ts';
+import type { SuccessResponse, AddressResponse, AddressCreateRequest } from '../../../types';
+
+export const useCreateAddress = () => {
+    const queryClient = useQueryClient()
+
+    return useMutation<SuccessResponse<AddressResponse>, Error, AddressCreateRequest>({
+        mutationFn: async (data) => {
+            const response = await Api.post('/api/address', data);
+            return response.data;
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['addresses'] });
+        }
+    });
+};
