71fd078a8a754b7a92d89dc7dcaab2b2b329e662
Arif14377 <arifrahman14377@gmail.com>
2026-08-02 11:52:34 +0700

    feat: add useAddToCart hook for adding items to web cart

---
+++ frontend/src/hooks/web/cart/useAddToCart.ts
@@ -0,0 +1,25 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import Api from '../../../services/api.ts';
+import type { SuccessResponse, CartItem } from '../../../types';
+
+interface AddToCartParam {
+    productId: number;
+    quantity: number;
+}
+
+export const useAddToCart = () => {
+    const queryClient = useQueryClient();
+
+    useMutation<SuccessResponse<CartItem>, Error, AddToCartParam>({
+        mutationFn: async ({ productId, quantity }) => {
+            const response = await Api.post('/api/carts', {
+                product_id: productId,
+                quantity
+            });
+            return response.data;
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({ queryKey: ['web-cart'] });
+        }
+    })
+}
