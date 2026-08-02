217a55f4b9a09600a897df4a466bdb4ce0e26cd5
Arif14377 <arifrahman14377@gmail.com>
2026-08-02 18:55:18 +0700

    feat: add useGetOrderDetail hook for fetching order details

---
+++ frontend/src/hooks/web/order/useGetOrderDetail.ts
@@ -0,0 +1,14 @@
+import { useQuery } from '@tanstack/react-query';
+import Api from '../../../services/api.ts';
+import type { SuccessResponse, OrderDetailResponse } from '../../../types';
+
+export const useGetOrderDetail = (id: string) => {
+    return useQuery<SuccessResponse<OrderDetailResponse>>({
+        queryKey: ['order', id],
+        queryFn: async () => {
+            const response = await Api.get(`/api/public/orders/${id}`);
+            return response.data;
+        },
+        enabled: !!id
+    });
+};
