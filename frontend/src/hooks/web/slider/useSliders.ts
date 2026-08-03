a73da6831b7e28f3ad5f0248885ac78f5239b334
Arif14377 <arifrahman14377@gmail.com>
2026-08-03 13:45:45 +0700

    feat: add useSliders hook for fetching slider data

---
+++ frontend/src/hooks/web/slider/useSliders.ts
@@ -0,0 +1,13 @@
+import { useQuery } from '@tanstack/react-query';
+import Api from '../../../services/api.ts';
+import type { Slider } from '../../../types';
+
+export const useSliders = () => {
+    return useQuery<Slider[]>({
+        queryKey: ['web-sliders'],
+        queryFn: async () => {
+            const response = await Api.get(`/api/public/sliders`);
+            return response.data
+        }
+    });
+};
