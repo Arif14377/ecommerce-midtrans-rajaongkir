39eaecdd4d92f37ef220181327eb14b1dbee63b4
Arif14377 <arifrahman14377@gmail.com>
2026-08-12 12:31:19 +0700

    refactor: encapsulate JWT key retrieval in getJWTKey function

--- backend/src/helpers/jwt.go
+++ backend/src/helpers/jwt.go
@@ -1,13 +1,21 @@
 package helpers
 
 import (
+	"errors"
 	"time"
 
 	"github.com/arif14377/ecommerce-midtrans-rajaongkir/src/config"
 	"github.com/golang-jwt/jwt/v5"
 )
 
-var jwtKey = []byte(config.GetEnv("JWT_SECRET"))
+func getJWTKey() ([]byte, error) {
+	secret := config.GetEnv("JWT_SECRET")
+	if secret == "" {
+		return nil, errors.New("JWT_SECRET is not configured")
+	}
+
+	return []byte(secret), nil
+}
 
 // GenerateToken membuat JWT untuk username dengan masa berlaku 24 jam.
 //
@@ -18,6 +26,10 @@ var jwtKey = []byte(config.GetEnv("JWT_SECRET"))
 //	token, err := helpers.GenerateToken(user.Username)
 func GenerateToken(username string) (string, error) {
 	expirationTime := time.Now().Add(24 * time.Hour)
+	jwtKey, err := getJWTKey()
+	if err != nil {
+		return "", err
+	}
 
 	claims := &jwt.RegisteredClaims{
 		Subject:   username,
@@ -43,6 +55,10 @@ func GenerateToken(username string) (string, error) {
 //	token, err := helpers.VerifyToken(tokenString)
 func VerifyToken(tokenString string) (*jwt.Token, error) {
 	claims := &jwt.RegisteredClaims{}
+	jwtKey, err := getJWTKey()
+	if err != nil {
+		return nil, err
+	}
 
 	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (any, error) {
 		return jwtKey, nil
