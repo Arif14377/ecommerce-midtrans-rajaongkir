4977bd0fcd9f5da8f0597e865ed7047f41f25873
Arif14377 <arifrahman14377@gmail.com>
2026-08-13 21:27:47 +0700

    fix: update `useSliders` hook to return the correct data structure from API response

--- frontend/src/hooks/web/slider/useSliders.ts
+++ frontend/src/hooks/web/slider/useSliders.ts
@@ -1,13 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
 import Api from '../../../services/api.ts';
-import type { Slider } from '../../../types';
+import type { SuccessResponse, Slider } from '../../../types';
 
 export const useSliders = () => {
-    return useQuery<Slider[]>({
+    return useQuery<SuccessResponse<Slider[]>>({
         queryKey: ['web-sliders'],
         queryFn: async () => {
             const response = await Api.get(`/api/public/sliders`);
-            return response.data
+            return response.data.data;
         }
     });
 };
