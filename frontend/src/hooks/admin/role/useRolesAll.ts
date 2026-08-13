ce55e0a33c316fcdb572a215b0387e481334ddd3
Arif14377 <arifrahman14377@gmail.com>
2026-08-13 14:49:45 +0700

    fix: update type annotations in useRolesAll hook for improved type safety

--- frontend/src/hooks/admin/role/useRolesAll.ts
+++ frontend/src/hooks/admin/role/useRolesAll.ts
@@ -1,13 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
 import Api from "../../../services/api";
-import type { RoleResponse } from "../../../types";
+import type { RoleResponse, SuccessResponse } from "../../../types";
 
 export const useRolesAll = () => {
-    return useQuery<RoleResponse, Error>({
-        queryKey: ['roles', 'all'],
-        queryFn: async () => {
-            const response = await Api.get('/api/admin/roles/all')
-            return response.data.data;
-        }
-    });
+  return useQuery<RoleResponse[], Error>({
+    queryKey: ['roles', 'all'],
+    queryFn: async () => {
+      const response = await Api.get<SuccessResponse<RoleResponse[]>>('/api/admin/roles/all');
+      return response.data.data;
+    }
+  });
 };
