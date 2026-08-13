2c886d594133f271523d28d8e360f19300763571
Arif14377 <arifrahman14377@gmail.com>
2026-08-13 12:35:44 +0700

    fix: correct permission create mutation response type

--- frontend/src/hooks/admin/permission/usePermissionCreate.ts
+++ frontend/src/hooks/admin/permission/usePermissionCreate.ts
@@ -1,12 +1,12 @@
 import { useMutation } from '@tanstack/react-query'
 import Api from '../../../services/api.ts';
-import type { PermissionCreateRequest } from '../../../types';
+import type { SuccessResponse, PermissionCreateRequest, PermissionResponse } from '../../../types';
 
 export const usePermissionCreate = () => {
-    return useMutation<PermissionCreateRequest, Error, PermissionCreateRequest>({
+    return useMutation<SuccessResponse<PermissionResponse>, Error, PermissionCreateRequest>({
         mutationFn: async (permissionData) => {
             const response = await Api.post('/api/admin/permissions', permissionData);
-            return response.data.data;
+            return response.data;
         }
     });
 };
