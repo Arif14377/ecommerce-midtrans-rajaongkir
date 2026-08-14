f8b46359882e27626d742e3e775a2fc7a68e443a
Arif14377 <arifrahman14377@gmail.com>
2026-08-15 03:27:09 +0700

    fix: update useSliders hook to return correct query type

--- frontend/src/hooks/web/slider/useSliders.ts
+++ frontend/src/hooks/web/slider/useSliders.ts
@@ -1,9 +1,9 @@
 import { useQuery } from '@tanstack/react-query';
 import Api from '../../../services/api.ts';
-import type { SuccessResponse, Slider } from '../../../types';
+import type { Slider } from '../../../types';
 
 export const useSliders = () => {
-    return useQuery<SuccessResponse<Slider[]>>({
+    return useQuery<Slider[]>({
         queryKey: ['web-sliders'],
         queryFn: async () => {
             const response = await Api.get(`/api/public/sliders`);
